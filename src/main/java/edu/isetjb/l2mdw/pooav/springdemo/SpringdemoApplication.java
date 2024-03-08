4a3a3031779b9591c07702e57ecb8f3daa27967c
Oussema <126197852+OussEmabouchahwa@users.noreply.github.com>
2024-03-08 20:09:06 +0100

    Add files via upload

---
+++ src/main/java/edu/isetjb/l2mdw/pooav/springdemo/SpringdemoApplication.java
@@ -0,0 +1,29 @@
+package edu.isetjb.l2mdw.pooav.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootApplication
+@RestController
+public class SpringdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return new ResponseEntity<>("An error occurred: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
